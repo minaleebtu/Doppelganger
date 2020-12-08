ce0b151dca2dd7c05bde3b92c80f895a59822425
minaleebtu <leemina@b-tu.de>
2020-12-08 22:11:00 +0100

    added language input for part2_task1

--- Part2/part2_task1.py
+++ Part2/part2_task1.py
@@ -3,23 +3,43 @@ from nltk.corpus import stopwords
 import pandas as pd
 import spacy
 
-#Download the package from nltk
 nltk.download("stopwords")
 nltk.download('punkt')
 nltk.download('wordnet')
 
-#Load the german language from spacy
-nlp = spacy.load('de_core_news_sm')
-#read the csv file
 data = pd.read_csv("Comments.csv")
-#filter the content that we want to process
 info = data["content"]
-#set german language and package from nltk
-stop = stopwords.words('german')
-
 result = pd.DataFrame(info)
 
-#Create a table comparison for the raw data and the converted one
-result['Content without stopwords'] = result['content'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop]))
-result['Lemmatized content'] = result['content'].apply(lambda x: ' '.join([token.lemma_ for token in nlp.tokenizer(x)]))
-print(result)
+lang = input("Enter your langauge (en for English, de for German): ")
+
+if lang == 'en':
+    # python -m spacy download en_core_web_sm
+    nlp = spacy.load('en_core_web_sm')
+
+    stop = stopwords.words('english')
+
+    result['Content without stopwords'] = result['content'].apply(
+        lambda x: ' '.join([word for word in x.split() if word.lower() not in stop]))
+    result['Lemmatized content'] = result['content'].apply(
+        lambda x: ' '.join([token.lemma_ for token in nlp.tokenizer(x)]))
+    print(result)
+
+    result.to_csv("Pre-processing.csv", header=['Content', 'Content without stopwords', 'Lemmatized content'],
+                  index=False)
+elif lang == 'de':
+    # python -m spacy download de_core_news_sm
+    nlp = spacy.load('de_core_news_sm')
+
+    stop = stopwords.words('german')
+
+    result['Content without stopwords'] = result['content'].apply(
+        lambda x: ' '.join([word for word in x.split() if word.lower() not in stop]))
+    result['Lemmatized content'] = result['content'].apply(
+        lambda x: ' '.join([token.lemma_ for token in nlp.tokenizer(x)]))
+    print(result)
+
+    result.to_csv("Pre-processing.csv", header=['Content', 'Content without stopwords', 'Lemmatized content'],
+                  index=False)
+else:
+    print("Unrecognised argument. Please specify 'en' or 'de'")
