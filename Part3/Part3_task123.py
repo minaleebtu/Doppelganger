8b9f3039f15e866e866124edfe980be591d2f925
minaleebtu <leemina@b-tu.de>
2021-01-05 23:01:09 +0100

    added input (author A and B)

--- Part3/Part3_task123.py
+++ Part3/Part3_task123.py
@@ -9,11 +9,6 @@ from sklearn.model_selection import LeaveOneGroupOut
 import joblib
 from joblib import Parallel, delayed
 from sklearn import svm
-# data = pd.read_csv("Comments.csv")
-#
-# df = pd.DataFrame([x for x in zip(data["username"], data["content"])], columns=['username', 'comment'])
-# # print(df)
-# dataList = df.reset_index(drop=True).values.tolist()
 
 # Load the new sorted dataset
 new = pd.read_csv('Joined_1.csv')
@@ -85,18 +80,19 @@ def getLabelOfUsers(username):
     usersdict = {}
     for index in range(len(userList)):
         users.append(userList[index])
+
     newUsers = list(set(users))
+
     le = LabelEncoder()
     encoded_users = le.fit_transform(newUsers)
     original_users = le.inverse_transform(encoded_users)
+
     for eu, ou in zip(encoded_users, original_users):
         usersdict.update({ou:eu})
-    print("usersdict: ", usersdict)
+    result = None
     for user, label in usersdict.items():
         if username == user:
             result = label
-            print("username en: ", result)
-
     return result
 
 
@@ -175,65 +171,84 @@ def getProbsTrainTest(clf, data, label, train, test, modeldir, saveModel):
     scores = clf.predict_proba(test_data)
     return scores
 
+
 # Create a function for calculating the combination of all probabilities
-def getCombinedProbs(selection, a, b, prob_per_author, allAuthors, encode_to_num, authors_name):
-    total_prob = {}
-    add_prob = {}
-    sq_prob = {}
+def getCombinedProbs(outfile, selection, a, b, prob_per_author):
+    # mul_prob = {}
+    # avg_prob = {}
+    # sq_prob = {}
 
-    # with open(outfile, "w+") as out:
-    #     out.write(
-    #         'Author 1, Author 2, P(A->B), P(B->A),Multiplication P(1->2)*P(2->1), Averaged (P(1->2)+P(2->1))/2, Squared (P(1->2)^2+P(2->1)^2)/2, Encode 1, Encode 2\n')
-    #     for i in range(len(allAuthors)):
-    #
-    #         a = int(allAuthors[i])
-    #         # if len(authors_to_numbers[a])==0:
-    #         #    continue
-    #         for j in range(i + 1, len(allAuthors)):
-    #             b = allAuthors[j]
-    #
-    result = 0
+    with open(outfile, "w+") as out:
+        out.write(
+            'Author 1, Author 2, P(A->B), P(B->A),Multiplication P(1->2)*P(2->1), Averaged (P(1->2)+P(2->1))/2, Squared (P(1->2)^2+P(2->1)^2)/2, Encode 1, Encode 2\n')
+        # for i in range(len(allAuthors)):
+        #
+        #     a = int(allAuthors[i])
+        #     # if len(authors_to_numbers[a])==0:
+        #     #    continue
+        #     for j in range(i + 1, len(allAuthors)):
+        #         b = allAuthors[j]
+        labelA = getLabelOfUsers(a)
+        labelB = getLabelOfUsers(b)
+        result = 0
 
-    total = prob_per_author[a][b] * prob_per_author[b][a]
-    addition = (prob_per_author[a][b] + prob_per_author[b][a]) / 2
-    sqsum = (prob_per_author[a][b] * prob_per_author[a][b] + prob_per_author[b][a] * prob_per_author[b][
-        a]) / 2
-    #
-    #             out.write(str(authors_name[a]) + " ," + str(authors_name[b]) + " ," +
-    #                       str(prob_per_author[a][b]) + "," + str(prob_per_author[b][a]) + "," +
-    #                       str(total) + "," + str(addition) + "," + str(sqsum) + "," +
-    #                       str(encode_to_num[a]) + " ," + str(encode_to_num[b]) +
-    #                       "\n")
-    #
-    #             # out.write(str(encode_to_num[a])+" ,"+str(encode_to_num[b])+" ,"+str(prob_per_author[a][b])+","+str(prob_per_author[b][a])+","+str(total)+","+str(addition)+","+str(sqsum)+" ,"+str(authors_name[a])+" ,"+str(authors_name[b])+"\n")
-    #
-    if total in total_prob.keys():
-        total_prob[total] += result
-    else:
-        total_prob[total] = result
+        multiple = prob_per_author[labelA][labelB] * prob_per_author[labelB][labelA]
+        average = (prob_per_author[labelA][labelB] + prob_per_author[labelB][labelA]) / 2
+        squared = (prob_per_author[labelA][labelB] * prob_per_author[labelA][labelB] + prob_per_author[labelB][labelA] * prob_per_author[labelB][labelA]) / 2
 
-    if addition in add_prob.keys():
-        add_prob[addition] += result
-    else:
-        add_prob[addition] = result
-    if sqsum in sq_prob.keys():
-        sq_prob[sqsum] += result
-    else:
-        sq_prob[sqsum] = result
+        out.write(a + " ," + b + " ," +
+                  str(prob_per_author[labelA][labelB]) + "," + str(prob_per_author[labelB][labelA]) + "," +
+                  str(multiple) + "," + str(average) + "," + str(squared) + "," +
+                  str(labelA) + " ," + str(labelB) +
+                  "\n")
+
+        # out.write(str(encode_to_num[a])+" ,"+str(encode_to_num[b])+" ,"+str(prob_per_author[a][b])+","+str(prob_per_author[b][a])+","+str(multiple)+","+str(average)+","+str(squared)+" ,"+str(authors_name[a])+" ,"+str(authors_name[b])+"\n")
+
+    # if multiple in mul_prob.keys():
+    #     mul_prob[multiple] += result
+    # else:
+    #     mul_prob[multiple] = result
     #
-    # out.close()
+    # if average in avg_prob.keys():
+    #     avg_prob[average] += result
+    # else:
+    #     avg_prob[average] = result
+    # if squared in sq_prob.keys():
+    #     sq_prob[squared] += result
+    # else:
+    #     sq_prob[squared] = result
+
+    out.close()
+
     if selection == 'multiple':
-        return total_prob
+        return multiple
     elif selection == 'average':
-        return add_prob
+        return average
     elif selection == 'squared':
-        return sq_prob
+        return squared
     # return total_prob, add_prob, sq_prob
 
 
 prob_per_author = getProbsGSThread(4, clf, DATA_PCA, y, allAuthors, 'models/', '100-w10-classifier.joblib.pkl')
 
 allAuthorNames = authors_to_num.keys()
+
+
+while True:
+    userA = input("Please enter the name of author A: ")
+    if getLabelOfUsers(userA) == None:
+        print("Please enter the valid name of author")
+    else:
+        break
+
+while True:
+    userB = input("Please enter the name of author B: ")
+
+    if getLabelOfUsers(userB) == None:
+        print("Please enter the valid name of author")
+    else:
+        break
+
 
 while True:
     selection = input("Please enter way to combine probabilities('multiple' for multiplication, 'average' for average 'squared' for squared average): ")
@@ -243,17 +258,7 @@ while True:
     else:
         break
 
-a = {'Super-Duper Missile': ' "Wenn Trump Jr. in die Schlagzeilen ger??t, weil er in der Mongolei ein unter besonderem Schutz stehendes Schaf erlegt, ohne eine Genehmigung daf??r zu haben, erheitert das viele Trump-Anh??nger sicherlich eher, als dass es sie abschreckt. " Und dieses asoziale Gehabe seiner Basis ist das eigentlich gef??hrliche was immer wieder in verschiedensten Situationen offensichtlich wird. Es geht einzig und allein um das eigene Weltbild. Verst??ndnis f??r andere, R??cksicht f??r andere - alles egal, hauptsache man kann den eigenen "Lebensstil" weiter leben. Mmn ist fehlende Bildung das gr????te Problem. Aber es ist durchaus so gewollt von der "eigenen" Partei. '}
-b = {'aaaaaaaaaaaaaaaasssssssssssssdddddddddd': ' "Wenn Trump Jr. in die Schlagzeilen ger??t, weil er in der Mongolei ein unter besonderem Schutz stehendes Schaf erlegt, ohne eine Genehmigung daf??r zu haben, erheitert das viele Trump-Anh??nger sicherlich eher, als dass es sie abschreckt. " Und dieses asoziale Gehabe seiner Basis ist das eigentlich gef??hrliche was immer wieder in verschiedensten Situationen offensichtlich wird. Es geht einzig und allein um das eigene Weltbild. Verst??ndnis f??r andere, R??cksicht f??r andere - alles egal, hauptsache man kann den eigenen "Lebensstil" weiter leben. Mmn ist fehlende Bildung das gr????te Problem. Aber es ist durchaus so gewollt von der "eigenen" Partei. '}
-# b = {'whitemouse': ' "Auch um Kraft zu sammeln, die wir brauchen um uns gegen die zu wehren, denen an der glimpflichen Bew??ltigung der Pandemie wenig gelegen ist. Aus welchen Gr??nden auch immer." Das wird schwierig,  wenn die immer wieder von Verwaltungsrichtern R??ckendeckung erhalten. In K??ln d??rfen die Menschen (zu Recht) nicht auf der Stra??e Karneval feiern, aber Wirrdenker d??rfen sich auf der Stra??e versammeln. W??hrend Karnevalisten sich noch bem??hen w??rden, die Abstands- und Maskenregeln einzuhalten,   bem??hen sich die Wirrdenker, ein m??glichst virenverbreitendes Verhalten an den Tag zu legen. Was daran sch??tzenswert sein soll, erschlie??t sich mir nicht. Und den Schwachsinn, den diese Leute von sich geben, hat nun mittlerweile jeder, der lesen  h??ren oder sehen kann, zur Kenntnis genommen. Ebenso, dass es einen gewissen Bodensatz der Gesellschaft gibt, der ihn vertritt. '}
-userA = ''.join(list(a.keys()))
-userB = ''.join(list(b.keys()))
-print("userA: ", userA)
-print("userB: ", userB)
-print("A: ", getLabelOfUsers(userA))
-print("A: ", getLabelOfUsers(userB))
-
-combined = getCombinedProbs(selection, getLabelOfUsers(userA), getLabelOfUsers(userB), prob_per_author, list(allAuthors), encode_to_num, list(allAuthorNames))
+combined = getCombinedProbs("results.csv", selection, userA, userB, prob_per_author)
 
 while True:
     threshold = input("Please enter the threshold (range: 0-1): ")
@@ -267,12 +272,10 @@ while True:
         except ValueError:
             print("This is not a number. Please enter a valid number")
 
-combinedValue = float(list(combined)[0])
+# combinedValue = float(list(combined)[0])
+print("Combined probability is ", combined)
 
-print("combined: ", type(combinedValue), combinedValue, combined)
-print("threshold: ", type(threshold), threshold)
-
-if combinedValue > threshold:
-    print("They are Doppelgangers")
+if combined > threshold:
+    print(">> They are Doppelgangers")
 else:
-    print("They are not Doppelgangers")
+    print(">> They are not Doppelgangers")
