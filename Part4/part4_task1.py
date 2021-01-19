8bac40f909db74050cb48382c0b0e13d93b91028
minaleebtu <leemina@b-tu.de>
2021-01-19 01:01:39 +0100

    removed unnecessary code and put some comments

--- Part4/part4_task1.py
+++ Part4/part4_task1.py
@@ -1,7 +1,6 @@
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn import svm
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import plot_confusion_matrix
@@ -9,52 +8,43 @@ import matplotlib.pyplot as plt
 # Generating accuracy, precision, recall and f1-score
 from sklearn.metrics import classification_report
 
-
 new = pd.read_csv('resultss.csv')
-pd.options.display.width = 0
 
-pd.options.display.float_format = "{:,.3f}".format
-
-rtr = new.drop([ 'Encode 1', 'Encode 2'], axis = 1)
-# rtr['Threshold'] = rtr.mean(axis=1)
+rtr = new.drop(['Encode 1', 'Encode 2'], axis=1)
+# Getting mean of combined probabilities (multipliction, average, squared average) per row as threshold
 rtr['Threshold'] = rtr.apply(lambda row: (row.Multiplication + row.Averaged + row.Squared)/3, axis=1)
+# Getting mean of threshold from all rows and compare with each threshold
+# if threshold is greater than mean of threshold from all rows, we can say two authors are doppelgangers (label: 1)
+rtr['Doppelgangers'] = rtr['Threshold'].apply(lambda x: 0 if x < rtr['Threshold'].mean() else 1)
 
-rtr['Doppelgangers'] = rtr['Threshold'].apply(lambda x: 0 if x <= round(rtr['Threshold'].mean(),3) else 1)
-countDoppel = rtr['Doppelgangers'].value_counts()
-print("countDoppel:\n", countDoppel)
-print("====================================================================================")
-# rtr['Threshold'] = rtr.apply(lambda row: row.Multiplication+ row.Averaged+row.Squared, axis=1)
-# rtr['Doppelgangers'] = rtr['Threshold'].apply(lambda x: '0' if x <= 0.024 else '1')
-
-X = rtr.drop(['Author 1', ' Author 2','Doppelgangers'], axis = 1)
+# Values of probabilities (dependent data)
+X = rtr.drop(['Author 1', ' Author 2', 'Doppelgangers'], axis=1)
+# Label of doppelganger (independent data)
 y = rtr['Doppelgangers']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+# Splitting data into train and test dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
-classifier = LogisticRegression()
+# Defining classifier and train dataset
+classifier = svm.SVC()
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-cm= confusion_matrix(y_test, y_pred)
-print(cm)
-print(cm.shape)
+# Getting the number of true negative, false positive, false negative, true positive
+cm = confusion_matrix(y_test, y_pred)
+print("True Negative: ", cm[0][0], "\nFalse Positive: ", cm[0][1])
+print("False Negative: ", cm[1][0], "\nTrue Positive: ", cm[1][1])
+print("====================================================================================")
 
-# clf.fit(X_train, y_train)
-# SVC(random_state=0)
-
+# Plotting the result
 plot_confusion_matrix(classifier, X_test, y_test)
 plt.show()
 
-tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-print(tn, fp, fn, tp)
+# tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+# print(tn, fp, fn, tp)
 
+# Values of precision, recall, F1 score for each class
 target_names = ['Non-Doppelganger', 'Doppelganger']
-print("====================================================================================")
 print(classification_report(y_test, y_pred, target_names=target_names))
-
-# print(new)
-# print(os.getcwd())
-
-
